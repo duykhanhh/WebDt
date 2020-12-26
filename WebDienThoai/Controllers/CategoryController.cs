b0acc21734ad5c38e222e4439d9a244baddf328f
vuduykhanh1999 <vuduykhanh1999@gmail.com>
2020-12-26 14:38:35 +0700

    test commit

--- WebDienThoai/Controllers/CategoryController.cs
+++ WebDienThoai/Controllers/CategoryController.cs
@@ -14,7 +14,7 @@ namespace WebDienThoai.Controllers
         {
 
             var model = new SanPhamDAO().ViewDetailByNSXID(id);
-            
+            // Tesst
             return View(model);
         }
     }
